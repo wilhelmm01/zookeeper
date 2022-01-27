4be6512637692812da30da3fa43d1b109590d906
Your Name <matt14w@gmail.com>
2022-01-26 19:51:14 -0500

    updated port

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const { animals} = require('./data/animals.json')
@@ -51,6 +52,6 @@ function filterByQuery(query, animalsArray) {
   });
 
 
-app.listen(3001, () => {
-    console.log('API server now on port 3001!')
-});
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
